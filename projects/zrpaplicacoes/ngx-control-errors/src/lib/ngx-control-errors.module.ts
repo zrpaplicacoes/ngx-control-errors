483ec1ca4278f7cf7d5366dd8052cecb5cb3d4f6
Ian Rodrigues <ian.rodrigues@zrp.com.br>
2024-02-23 18:29:26 -0300

    fix(lib/ngx-control): return type adjusted to satisfy Angular 13

--- projects/zrpaplicacoes/ngx-control-errors/src/lib/ngx-control-errors.module.ts
+++ projects/zrpaplicacoes/ngx-control-errors/src/lib/ngx-control-errors.module.ts
@@ -1,15 +1,14 @@
-import { NgModule } from '@angular/core';
-import { NgxControlErrorsComponent } from './components/ngx-control-errors.component';
-import { ModuleWithProviders } from '@angular/compiler/src/core';
-import { NgxFormSubmitDirective } from './directives/ngx-form-submit.directive';
-import { NgxControlErrorsDirective } from './directives/ngx-control-errors.directive';
 import { CommonModule } from '@angular/common';
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { NgxControlErrorsComponent } from './components/ngx-control-errors.component';
 import {
-  NgxControlErrors,
-  defaultErrors,
-  NgxControlErrorsOptions,
   NgxControlErrorOptions,
+  NgxControlErrors,
+  NgxControlErrorsOptions,
+  defaultErrors,
 } from './constants';
+import { NgxControlErrorsDirective } from './directives/ngx-control-errors.directive';
+import { NgxFormSubmitDirective } from './directives/ngx-form-submit.directive';
 
 @NgModule({
   declarations: [
@@ -25,7 +24,7 @@ import {
   ],
 })
 export class NgxControlErrorsModule {
-  static forRoot(config?: NgxControlErrorOptions): ModuleWithProviders {
+  static forRoot(config?: NgxControlErrorOptions): ModuleWithProviders<NgxControlErrorsModule> {
     return {
       ngModule: NgxControlErrorsModule,
       providers: [
